09037d58124af787ccbca8b1c593c624d3a7363b
Yosept <yosept.flores@gmail.com>
2020-02-06 14:23:54 -0600

    We are strugling with some trouble at the comment creation

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_02_06_181520) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
