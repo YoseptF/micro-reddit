be0d5c13c206cd21d32777c16b7e313093f7751f
Yosept <yosept.flores@gmail.com>
2020-02-06 13:28:17 -0600

    We added the linter files, and also created the models, now I'm pushing to let my coding partner see the changes while we eat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  name: 'Joseph',
+  password_digest: '123123',
+  email: 'yosept.flores@gmail.com'
+)
+
+Post.create(
+  title: 'my first post',
+  content: 'the content'
+)
+
+Comment.create(
+  content: 'message, content',
+  user_id: 1,
+  post_id: 1
+)
