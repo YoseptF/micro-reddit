28c381b9d58d53b379c02f1974e949692c7489d4
Yosept <yosept.flores@gmail.com>
2020-02-07 10:27:56 -0600

    The tests run now. The problem was on the id's cerated by the test env.

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+  def setup
+    @user = User.create(name: 'joseph', password_digest: 'lol', email: 'mail@m.com')
+    @post = Post.create(title: 'dog', content: 'cat', user_id: @user.id)
+  end
   # test "the truth" do
   #   assert true
   # end
+  test 'valid user' do
+    assert @post.valid?
+    p User.all
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: 'joseph', password_digest: 'lol', email: 'mail@m.com')
+  end
   # test "the truth" do
   #   assert true
   # end
+  test 'valid user' do
+    assert @user.valid?
+  end
 end
